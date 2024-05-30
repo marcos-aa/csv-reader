14ab09966988a86c6fb1849d657964ac4a4bcb1d
Marcos <marcosandrade.it@gmail.com>
2024-05-30 17:19:34 -0300

    UI: stylized card information

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import { faBasketball, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 function App() {
   return (
@@ -11,7 +14,20 @@ function App() {
       </div>
 
       <section id={"cards-holder"}>
-        <div className="info-card" data-testid="info-card"></div>
+        <div className="info-card" data-testid="info-card">
+          <div className="info-item">
+            <FontAwesomeIcon icon={faUser} />
+            <h1 className="info-item">John Doe</h1>
+          </div>
+          <div className="info-item">
+            <FontAwesomeIcon icon={faHome} />
+            <p className="info-item"> New York, USA </p>
+          </div>
+          <div className="info-item">
+            <FontAwesomeIcon icon={faBasketball} />
+            <p className="info-item"> Baseball </p>
+          </div>
+        </div>
         <div className="info-card" data-testid="info-card"></div>
         <div className="info-card" data-testid="info-card"></div>
         <div className="info-card" data-testid="info-card"></div>
