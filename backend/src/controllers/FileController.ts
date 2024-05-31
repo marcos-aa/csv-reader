b90c7c24f8ea4a591fb02b61bf3d876eea4728aa
Marcos <marcosandrade.it@gmail.com>
2024-05-31 16:12:07 -0300

    Refactor: include 'message' key in response object returned from /api/files

--- backend/src/controllers/FileController.ts
+++ backend/src/controllers/FileController.ts
@@ -3,8 +3,8 @@ import FileServices from "../services/FileServices";
 
 export default class FileController {
   async create(req: Request, res: Response) {
-    if (!req.file) return res.status(500).json({ message: "Invalid file" });
-    const result = await new FileServices().create(req.file);
-    return res.status(200).json(result);
+    if (!req.file) return res.status(422).json({ message: "Invalid file" });
+    const message = await new FileServices().create(req.file);
+    return res.status(200).json({ message });
   }
 }
