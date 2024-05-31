834b8dd0b1796188fa0ec04d1a2f450ee81b1a48
Marcos <marcosandrade.it@gmail.com>
2024-05-31 08:38:21 -0300

    Implemented file change handler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,40 @@
-import { faBasketball, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBasketball,
+  faUpload,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FormEvent, useState } from "react";
 import "./App.css";
 function App() {
+  const [file, setFile] = useState<File | null>();
+
+  const handleFileChange = (e: FormEvent<HTMLInputElement>) => {
+    const file = e.currentTarget.files?.item(0);
+    setFile((prev) => file ?? prev);
+  };
+
   return (
     <main>
       <div id="actionables">
         <input name="search" type="text" value={""} placeholder="Search" />
         <label>
-          <input type="file" />
+          <input type="file" accept="text/csv" onChange={handleFileChange} />
           Upload
         </label>
+        <div id="file-actions" className={file ? "shared-wd" : "full-wd"}>
+          <button type="button" title="Remove CSV file" id="eject-file">
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+          <button type="submit" title="Submit CSV file">
+            <FontAwesomeIcon icon={faUpload} />
+          </button>
+        </div>
       </div>
 
-      <section id={"cards-holder"}>
+      <section id="cards-holder">
         <div className="info-card" data-testid="info-card">
           <div className="info-item">
             <FontAwesomeIcon icon={faUser} />
@@ -28,6 +49,7 @@ function App() {
             <p className="info-item"> Baseball </p>
           </div>
         </div>
+
         <div className="info-card" data-testid="info-card"></div>
         <div className="info-card" data-testid="info-card"></div>
         <div className="info-card" data-testid="info-card"></div>
