0983b135bf1b234d61c84f2d7bfb90871a288fbb
Marcos <marcosandrade.it@gmail.com>
2024-06-20 21:25:48 -0300

    Specified port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import app from "./app";
-
-const server = app.listen(3000);
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
 export default server;
