70ee7dfbdb0df184bcdbb1e3cdbba6dd8ed46119
Marcos <marcosandrade.it@gmail.com>
2024-11-15 12:43:54 -0300

    Hosted server on fly.io

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import UploadLabel from "./components/UploadLabel";
 
 const baseURL =
   import.meta.env.MODE === "production"
-    ? "https://csviewer.vercel.app"
+    ? "https://csviewer.fly.dev"
     : "http://localhost:3000";
 
 function App() {
